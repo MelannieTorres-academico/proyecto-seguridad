45aced2b42ad71d6c510b71565a898fc099e29b0
MartinCSans <a01700042@itesm.mx>
2019-02-28 22:08:42 -0600

    TestHash.py

--- TestHash.py
+++ TestHash.py
@@ -10,14 +10,36 @@ import hashlib
 from time import time
     
 def timer():
+    mssg = ["", "a", "abc", "message digest", "abcdefghijklmnopqrstuvwxyz", "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", "A...Za...z0...9", '1234567890' * 8, 'a' * 1000000]
+    #MD5
     start_time = time()
     for i in range(1000):
-        mssg = ["", "a", "abc", "message digest", "abcdefghijklmnopqrstuvwxyz", "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", "A...Za...z0...9", '1234567890' * 8, 'a' * 1000000]
+        
         for pos in mssg:
             hashlib.md5(pos.encode('utf-8'))
             
     elapsed_time = (time() - start_time)/1000
-    print("Elapsed time: %.10f seconds." % elapsed_time)
+    print("\n" + "Elapsed time: %.10f seconds." % elapsed_time)
+    
+    #SHA1
+    start_time = time()
+    for i in range(1000):
+        
+        for pos in mssg:
+            hashlib.sha1(pos.encode('utf-8'))
+            
+    elapsed_time = (time() - start_time)/1000
+    print("\n" + "Elapsed time: %.10f seconds." % elapsed_time)
+    
+    #SHA2
+    start_time = time()
+    for i in range(1000):
+        
+        for pos in mssg:
+            hashlib.sha256(pos.encode('utf-8'))
+            
+    elapsed_time = (time() - start_time)/1000
+    print("\n" + "Elapsed time: %.10f seconds." % elapsed_time)
     
 
 def main():
