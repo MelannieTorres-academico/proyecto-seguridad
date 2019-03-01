d0b300aea47a2fe3d8bf711406ed968697ddabac
Gesara01 <a01700777@itesm.mx>
2019-02-28 21:29:52 -0600

    Update decrypt.py
    DES test ready

--- decrypt.py
+++ decrypt.py
@@ -13,20 +13,17 @@ def arc4(key, message):
     msg = nonce + cipher.decrypt(message)
     return msg
 
-
 #des
 #https://www.dlitz.net/software/pycrypto/api/current/Crypto.Cipher.DES-module.html
 
-# from Crypto.Cipher import DES3
-# from Crypto import Random
-#
-# def des():
-#     key = b'Sixteen byte key'
-#     iv = Random.new().read(DES3.block_size)
-#     cipher = DES3.new(key, DES3.MODE_OFB, iv)
-#     plaintext = b'sona si latine loqueris '
-#     msg = iv + cipher.encrypt(plaintext)
-#     print(msg)
+from Crypto.Cipher import DES3
+from Crypto import Random
+
+def des(key, plaintext):
+    iv = Random.new().read(DES3.block_size)
+    cipher = DES3.new(key, DES3.MODE_OFB, iv)
+    msg = iv + cipher.encrypt(plaintext)
+    print(msg.hex())
 
 
 #aes
@@ -40,21 +37,31 @@ def main():
 
     key_sizes = [40, 56, 64, 80, 128, 192, 256]
 
+
+    print('RC4 tests')
     for i in range(0, len(keys_tests)):
-        print('Key size: ', key_sizes[i], end=' ')
+        print('Key size: ', key_sizes[i],' bits', end=' ')
         test_arc4(keys_tests[i])
+
+    print('DES tests')
+    test_des(b'8000000000000000')
 
 
 def test_arc4(key_string):
     key = bytearray.fromhex(key_string)
     start_time = time()
     for i in range(1000):
-        result = arc4(key,  b'0')
+        result = arc4(key,  b'Y\x81T]\xf14\n\xa4\xa1\xf7\x0f\xc7Ll\xc0\xb7m')
     elapsed_time = (time() - start_time)/1000
     print("Elapsed time: %.10f seconds." % elapsed_time)
 
-
-
+def test_des(key):
+    start_time = time()
+    for i in range(1000):
+        result = arc4(key,  b'95A8D72813DAA94D')
+    elapsed_time = (time() - start_time)/1000
+    print('Key size: 64 bits', end=' ')
+    print("Elapsed time: %.10f seconds." % elapsed_time)
 
 if __name__ == "__main__":
     main()
